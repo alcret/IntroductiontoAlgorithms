dda67bf5ebb7f44e0e6c194ba4db8422fdcaf40a
zhangchuang <mrzhangchuang@gmail.com>
2019-06-18 09:00:53 +0800

    分治法

--- src/MERGE/MERGE_SORT_test.go
+++ src/MERGE/MERGE_SORT_test.go
@@ -7,12 +7,8 @@ import (
 
 func TestMERGE(t *testing.T) {
 	var ii []int = []int{2, 4, 5, 7, 1, 2, 3}
-	var iii []int = []int{2, 4, 5, 7, 1, 2, 3}
-	//Merge(ii, 0, 3, len(ii)-1)
-	//var ii []int = []int{5, 2, 4, 7, 1, 3, 2, 6}
-	Merge_Sort(ii, 0, len(ii)-1)
 	//var iii []int = []int{5, 2, 4, 7, 1, 3, 2, 6}
-
-	//Merge_Sort(ii,0,len(ii))
-	fmt.Println(ii, iii)
+	//Merge(ii, 0, 3, len(ii)-1)
+	Merge_Sort(ii, 0, len(ii)-1)
+	fmt.Println(ii)
 }
