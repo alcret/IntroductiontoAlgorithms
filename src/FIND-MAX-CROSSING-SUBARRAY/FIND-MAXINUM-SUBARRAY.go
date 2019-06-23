c210938088bb365259c6e6636c992c84d274ecbc
zhangchuang <mrzhangchuang@gmail.com>
2019-06-23 13:55:46 +0800

    整理第一部分

--- src/FIND-MAX-CROSSING-SUBARRAY/FIND-MAXINUM-SUBARRAY.go
+++ src/FIND-MAX-CROSSING-SUBARRAY/FIND-MAXINUM-SUBARRAY.go
@@ -10,13 +10,14 @@ func Find_Maximum_SubArray(array []int, low, high int) (rlow, rhigh, sum int) {
 		cross_low, cross_high, cross_sum := Find_Max_Crossing_SubArray(array, low, mid, high)
 
 		if left_sum >= right_sum && left_sum >= cross_sum {
+
 			return left_low, left_high, left_sum
 		} else if right_sum >= left_sum && right_sum >= cross_sum {
+
 			return right_low, right_high, right_sum
 		} else {
+
 			return cross_low, cross_high, cross_sum
 		}
-
 	}
-
 }
--- src/FIND-MAX-CROSSING-SUBARRAY/FIND_MAX_CROSS_SUBARRAY_test.go
+++ src/FIND-MAX-CROSSING-SUBARRAY/FIND_MAX_CROSS_SUBARRAY_test.go
@@ -8,5 +8,4 @@ import (
 func TestFind_Max_Crossing_SubArray(t *testing.T) {
 	array := []int{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7}
 	fmt.Println(Find_Maximum_SubArray(array, 0, len(array)-1))
-
 }
